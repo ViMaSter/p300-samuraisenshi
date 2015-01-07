00bda11af8a7227f63d41d282be46ba14b8b6479
Vincent Mahnke <accounts@vimaster.de>
2015-01-07 02:50:29 +0100

    + Rudimentary player death + Rudimentary reset-screen + HUD-State

--- unreal/Source/samuraisenshi/Public/HUD_Ingame.h
+++ unreal/Source/samuraisenshi/Public/HUD_Ingame.h
@@ -5,14 +5,25 @@
 #include "GameFramework/HUD.h"
 #include "HUD_Ingame.generated.h"
 
-/**
- * 
- */
+UENUM()
+enum EInterfacePhase
+{
+	InterfacePhase_Intro,
+	InterfacePhase_Ingame,
+	InterfacePhase_Dead
+};
+
 UCLASS()
 class SAMURAISENSHI_API AHUD_Ingame : public AHUD
 {
 	GENERATED_UCLASS_BODY()
 
-	
-	
+	UPROPERTY(BlueprintReadOnly, Category = Interface)
+	TEnumAsByte<EInterfacePhase> CurrentInterfacePhase;
+
+	UFUNCTION(BlueprintCallable, Category = Interface)
+	void UpdateInterfacePhase(EInterfacePhase newInterfacePhase);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = Interface)
+	virtual void OnUpdateInterfacePhase(EInterfacePhase newInterfacePhase);
 };
