de04382a71c33fe438a0dbd452dc544d86fd2e40
Vincent Mahnke <accounts@vimaster.de>
2015-01-12 01:27:54 +0100

    + Splashscreen + Debug heal-feature * Rearranged files

---
+++ unreal/Source/samuraisenshi/Private/HUD_Start.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "samuraisenshi.h"
+#include "HUD_Start.h"
+
+
+AHUD_Start::AHUD_Start(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ unreal/Source/samuraisenshi/Public/HUD_Start.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/HUD.h"
+#include "HUD_Start.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAMURAISENSHI_API AHUD_Start : public AHUD
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
